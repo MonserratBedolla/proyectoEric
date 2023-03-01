c7f0d5c00eb03532b3e55b7f513cbf7787f2a6f8
MonserratBedolla <99926583+MonserratBedolla@users.noreply.github.com>
2023-02-28 20:05:39 -0600

    Add files via upload

---
+++ Carreras_Eric/sketch.js
@@ -0,0 +1,46 @@
+var canvas;
+var backgroundImage, bgImg, car1_img, car2_img, track;
+var database, gameState;
+var form, player, playerCount;
+var allPlayers, car1, car2,fuels, powerCoins;
+var cars = [];
+//declarar fuels y powercoins
+
+var fuels,powerCoins;
+
+function preload() {
+  backgroundImage = loadImage("./assets/background.png");
+  car1_img = loadImage("../assets/car1.png");
+  car2_img = loadImage("../assets/car2.png");
+  track = loadImage("../assets/track.jpg");
+  //recuerda que las imagenes se guardan en lugares diferentes a los sprites
+
+  powerCoinsImage = loadImage("../assets/goldCoin.png");
+  fuelImage = loadImage("../assets/fuel.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+ 
+}
+
+function draw() {
+  background(backgroundImage);
+  
+  if(playerCount===2){
+    game.update(1);
+  }
+
+  if (gameState === 1) {
+    game.play();
+  }
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
